4c24d122490768076f908fc608b417c3cd6060b8
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-08-24 13:05:01 +0300

    I've creatd class for saving data in xml file. This class is entity for new xml file.

---
+++ ETIM-API-Saver/Common/DTO/EtimFeaturesAndValuesXmlFileEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Common.DTO
+{
+    public class EtimFeaturesAndValuesXmlFileEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Description { get; set; } = string.Empty;
+        public string ContactInformation { get; set; } = string.Empty;
+
+        public int NumberOfEtimFeatures { get; set; }
+        public int NumberOfEtimValues { get; set; }
+
+        public Feature[] EtimFeatures { get; set; } = new Feature[0];
+        public Value[] EtimValues { get; set; } = new Value[0];
+    }
+}
