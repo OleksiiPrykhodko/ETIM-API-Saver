79771d6498c4e04a0cea355138394ebd0f99e491
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-08-24 13:42:44 +0300

    I've added loading of Values and saving it to the xml file.

--- ETIM-API-Saver/ETIM-API-Saver/Program.cs
+++ ETIM-API-Saver/ETIM-API-Saver/Program.cs
@@ -7,7 +7,7 @@ using Common.DTO.Request;
 using Common.DTO.Result;
 
 
-Console.WriteLine("Hello, I am ETIM API Saver!");
+Console.WriteLine("\tHello, I am ETIM API Saver!");
 Console.WriteLine();
 
 const string _authorizationUri = "https://etimauth.etim-international.com/connect/token";
@@ -18,15 +18,21 @@ const string _pathToFilesDirectory = @"./Generated files";
 
 string _authorization = "Basic c2llbWVuc191YTpYMGV5Y0hRWXFjUW9neU5GZ1ZRQ3lE";
 
+string _token = "";
+
 HttpClient _httpClient = new HttpClient();
 
 var _listOfAllFeatures = new List<Feature>();
+var _listofAllValues = new List<Value>();
 
 int _totalNumberOfFeatures = 0;
+int _totalNumberOfValues = 0;
+
 int _numberOfAlreadyLoadedFeatures = 0;
+int _numberOfAlreadyLoadedValues = 0;
 
 // STEP ONE - get access token ======================================================
-Console.WriteLine("Authorization step is started.");
+Console.WriteLine("\tAuthorization step is started.");
 
 HttpRequestMessage _httpPostAuthorizationRequest = new HttpRequestMessage(HttpMethod.Post, _authorizationUri)
 {
@@ -64,6 +70,7 @@ if (_authorizationResponse is not null && _authorizationResponse.IsSuccessStatus
     try
     {
         _authorizationResult = _authorizationResponse.Content.ReadFromJsonAsync<ResultOfRequestAccessTokenDTO>().Result;
+        _token = $"Bearer {_authorizationResult?.Access_token ?? "no token"}";
     }
     catch (NotSupportedException)
     {
@@ -90,14 +97,12 @@ else
     Console.WriteLine("Authorization is failed.");
 }
 
-Console.WriteLine("Authorization step is ended.");
+Console.WriteLine("\tAuthorization step is ended.");
 Console.WriteLine();
 
 
-// STEP TWO - get data ======================================================
-Console.WriteLine("Data loading step is started.");
-
-
+// STEP TWO - get Features ======================================================
+Console.WriteLine("\tLoading of Features step is started.");
 
 if (_authorizationResult is not null)
 {
@@ -122,7 +127,6 @@ if (_authorizationResult is not null)
             Content = JsonContent.Create(_jsonRequest)
         };
 
-        string _token = $"Bearer {_authorizationResult?.Access_token ?? "no token"}";
         _httpPostRequest.Headers.Add("Authorization", _token);
 
         HttpResponseMessage? _response = null;
@@ -195,16 +199,120 @@ if (_authorizationResult is not null)
 }
 else
 {
-    Console.WriteLine("Data loading step skipped. Downloading is not possible without authorization!");
+    Console.WriteLine("Features loading step skipped. Downloading is not possible without authorization!");
 }
 
-Console.WriteLine("Data loading step is ended.");
+Console.WriteLine("\tFeatures loading step is ended.");
 Console.WriteLine();
 
 
-// STEP THREE - save data to file ======================================================
-Console.WriteLine("Data in file saving step is started.");
+// STEP THREE - get Values ======================================================
+Console.WriteLine("\tLoading of Values step is started.");
 
+if (_authorizationResult is not null)
+{
+    do
+    {
+
+        var _jsonRequest = new RequestAllValuesWithMaximumDetailsAPIv2DTO()
+        {
+            From = _numberOfAlreadyLoadedValues,
+            Size = 1000,
+            Languagecode = "EN",
+            Deprecated = false,
+            Include = new Include
+            {
+                Descriptions = true,
+                Translations = false
+            }
+        };
+
+        var _httpPostRequest = new HttpRequestMessage(HttpMethod.Post, _valueUri)
+        {
+            Content = JsonContent.Create(_jsonRequest)
+        };
+
+        _httpPostRequest.Headers.Add("Authorization", _token);
+
+        HttpResponseMessage? _response = null;
+
+        try
+        {
+            _response = _httpClient.SendAsync(_httpPostRequest).Result;
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error! {e.Message}");
+            Console.WriteLine("Check internet connection and try again!");
+        }
+        finally
+        {
+            Console.ResetColor();
+        }
+
+        ResultOfRequestAllValuesWithMaximumDetailsAPIv2DTO? _result = null;
+
+        if (_response is not null && _response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Data was loaded successful.");
+            try
+            {
+                _result = _response.Content.ReadFromJsonAsync<ResultOfRequestAllValuesWithMaximumDetailsAPIv2DTO>().Result;
+            }
+            catch (NotSupportedException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error! Content type is not supported.");
+            }
+            catch (JsonException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error! Invalid json.");
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error! {e.Message}");
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Data loading is failed.");
+            break;
+        }
+
+        if (_result is not null)
+        {
+            _totalNumberOfValues = _result.Total;
+            _listofAllValues.AddRange(_result.Values);
+            _numberOfAlreadyLoadedValues = _listofAllValues.Count;
+        }
+        else
+        {
+            break;
+        }
+
+    }
+    while (_numberOfAlreadyLoadedValues < _totalNumberOfValues);
+
+    Console.WriteLine($"{_totalNumberOfValues}/{_numberOfAlreadyLoadedValues} - Values (total number / loaded).");
+}
+else
+{
+    Console.WriteLine("Values loading step skipped. Downloading is not possible without authorization!");
+}
+
+Console.WriteLine("\tValues loading step is ended.");
+Console.WriteLine();
+
+
+// STEP FOUR - save data to file ======================================================
+Console.WriteLine("\tData in file saving step is started.");
 
 // If directory does not exist, create it
 if (!Directory.Exists(_pathToFilesDirectory))
@@ -220,10 +328,12 @@ var _xmlFileEntity = new EtimFeaturesAndValuesXmlFileEntity()
     Description = "File with actual ETIM Features and Values for offline using.",
     ContactInformation = "https://www.linkedin.com/in/oleksiiprykhodko",
     NumberOfEtimFeatures = _listOfAllFeatures.Count,
-    EtimFeatures = _listOfAllFeatures.ToArray()
+    NumberOfEtimValues = _listofAllValues.Count,
+    EtimFeatures = _listOfAllFeatures.ToArray(),
+    EtimValues = _listofAllValues.ToArray()
 };
 
-var _fileName = $"ETIM Features and Values {DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year} {DateTime.Now.Hour}-{DateTime.Now.Minute}-{DateTime.Now.Second}.xml";
+var _fileName = $"ETIM Features and Values {DateTime.Now.Year}.{DateTime.Now.Month}.{DateTime.Now.Day} {DateTime.Now.Hour}-{DateTime.Now.Minute}-{DateTime.Now.Second}.xml";
 var _pathToXmlFile = $"{_pathToFilesDirectory}/{_fileName}";
 
 var _xmlSerializer = new XmlSerializer(typeof(EtimFeaturesAndValuesXmlFileEntity));
@@ -245,10 +355,10 @@ finally
     Console.ResetColor();
 }
 
-Console.WriteLine("Data in file saving step is ended.");
+Console.WriteLine("\tData in file saving step is ended.");
 Console.WriteLine();
 
-Console.WriteLine("The End");
+Console.WriteLine("\tThe End");
 Console.ReadKey();
 
 
