24ef594ae94ef51ba97f8129f666c6b24943270e
OleksiiPrykhodko <o.d.prykhodko@gmail.com>
2023-08-25 15:44:42 +0300

    I've added to ApiRequestService new method GetValues();

--- ETIM-API-Saver/BLL/Services/ApiRequestService.cs
+++ ETIM-API-Saver/BLL/Services/ApiRequestService.cs
@@ -13,24 +13,30 @@ namespace BLL.Services
 {
     public class ApiRequestService
     {
-        private const string _featureUri = "https://etimapi.etim-international.com/api/v2/Feature/Search";
+        private const string _featuresApiUri = "https://etimapi.etim-international.com/api/v2/Feature/Search";
+        const string _valueUri = "https://etimapi.etim-international.com/api/v2/Value/Search";
 
         private readonly HttpClient _httpClient;
-
-        public ApiRequestService(HttpClient httpClient)
+        private string _token;
+        public ApiRequestService(HttpClient httpClient, string token)
         {
             _httpClient = httpClient;
+            _token = token;
         }
 
-        public ResultOfRequestAllFeatureWithMaximumDetailsAPIv2DTO GetFeatures(RequestAllFeatureWithMaximumDetailsAPIv2DTO request, string token)
+        public void SetToken(string token)
         {
+            _token = token;
+        }
 
-            var httpPostRequest = new HttpRequestMessage(HttpMethod.Post, _featureUri)
+        public ResultOfRequestAllFeatureWithMaximumDetailsAPIv2DTO GetFeatures(RequestAllFeatureWithMaximumDetailsAPIv2DTO request)
+        {
+            var httpPostRequest = new HttpRequestMessage(HttpMethod.Post, _featuresApiUri)
             {
                 Content = JsonContent.Create(request)
             };
 
-            httpPostRequest.Headers.Add("Authorization", token);
+            httpPostRequest.Headers.Add("Authorization", _token);
 
             HttpResponseMessage response = _httpClient.SendAsync(httpPostRequest).Result;
 
@@ -43,6 +49,34 @@ namespace BLL.Services
             if (response is not null && response.IsSuccessStatusCode)
             {
                 var result = response.Content.ReadFromJsonAsync<ResultOfRequestAllFeatureWithMaximumDetailsAPIv2DTO>().Result;
+                return result ?? badResult;
+            }
+            else
+            {
+                return badResult;
+            }
+        }
+
+        public ResultOfRequestAllValuesWithMaximumDetailsAPIv2DTO GetValues(RequestAllValuesWithMaximumDetailsAPIv2DTO request)
+        {
+            var httpPostRequest = new HttpRequestMessage(HttpMethod.Post, _valueUri)
+            {
+                Content = JsonContent.Create(request)
+            };
+
+            httpPostRequest.Headers.Add("Authorization", _token);
+
+            HttpResponseMessage response = _httpClient.SendAsync(httpPostRequest).Result;
+
+            var badResult = new ResultOfRequestAllValuesWithMaximumDetailsAPIv2DTO()
+            {
+                Total = 0,
+                Values = new Value[0]
+            };
+
+            if (response is not null && response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadFromJsonAsync<ResultOfRequestAllValuesWithMaximumDetailsAPIv2DTO>().Result;
                 return result ?? badResult;
             }
             else
